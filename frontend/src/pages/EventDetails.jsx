c6aac78a144bfd652d1cceeff9b0906f72405128
Mohammad-Wadi <hamdo.wadii@gmail.com>
2025-10-14 21:16:08 +0200

    fix the logout page

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -9,7 +9,7 @@ const EventDetails = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "https://backend-events-api.onrender.com";
 
   useEffect(() => {
     fetchEventDetails();
