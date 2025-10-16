2a5ccadebe4782eed838f22146daa8b03b1cc86b
Boris Ket <56203567+keboris@users.noreply.github.com>
2025-10-16 09:54:55 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -153,7 +153,7 @@ const Header = () => {
                   </svg>
                   <span className="hidden md:inline">Create Event</span>
                 </Link>
-                <Link to="/my-events" className="btn btn-secondary btn-sm">
+                <Link to="/dashboard" className="btn btn-secondary btn-sm">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -168,7 +168,7 @@ const Header = () => {
                       d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
                     />
                   </svg>
-                  My Events
+                  Dashboard
                 </Link>
 
                 <button
