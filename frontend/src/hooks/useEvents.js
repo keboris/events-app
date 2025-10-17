25afdc7432cc679739a3e8b0cdfee19e09f5ccb9
Boris Ket <keboris@gmail.com>
2025-10-17 09:05:20 +0200

    Last Changes 5

---
+++ frontend/src/hooks/useEvents.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { EVENTS_ENDPOINT } from "../config/api";
+
+export const useEvents = () => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchAllEvents = async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(EVENTS_ENDPOINT);
+      if (!res.ok) throw new Error("Something went wrong");
+      const data = await res.json();
+      setEvents(data.results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllEvents();
+
+    // Listen for event changes (create, update, delete)
+    window.addEventListener("eventsChange", fetchAllEvents);
+    return () => {
+      window.removeEventListener("eventsChange", fetchAllEvents);
+    };
+  }, []);
+
+  return { events, isLoading };
+};
