16fc800aedcc6873966ff8d5d9a5f6d95b6b5cf6
Boris Ket <keboris@gmail.com>
2025-10-17 10:34:04 +0200

    Korrektur

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import AuthContextProvider from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*<AuthContextProvider>*/}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
-    {/* </AuthContextProvider>*/}
   </StrictMode>
 );
